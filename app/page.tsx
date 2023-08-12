959f8550d7f9a557f140a494cee066fa7e54ef88
Marcel Weber <mrweber@gmx.ch>
2023-08-12 18:29:37 +0200

    Placeholder text

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main>
       <h1 className={s.h1}>schussfreude.ch</h1>
+      <p className={s.p}>Vorschau der neuen schussfreude.ch-Seite.</p>
     </main>
   )
 }
