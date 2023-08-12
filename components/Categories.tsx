488fef82785ea6355ab91ab6690f00f227c8e23a
Marcel Weber <mrweber@gmx.ch>
2023-08-12 18:56:32 +0200

    Created Categories component and styling

---
+++ components/Categories.tsx
@@ -0,0 +1,13 @@
+import s from "@/styles/Categories.module.css"
+
+export default function Categories(){
+    return(
+        <div className={s.container}>
+            <div className={s.item}></div>
+            <div className={s.item}></div>
+            <div className={s.item}></div>
+            <div className={s.item}></div>
+            <div className={s.item}></div>
+        </div>
+    )
+}
