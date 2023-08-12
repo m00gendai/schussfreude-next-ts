1c1c57a059d636cb5f89b3e2d88365166333edf2
Marcel Weber <mrweber@gmx.ch>
2023-08-12 18:57:06 +0200

    Added HEro and Categories Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import s from "@/styles/frontpage.module.css"
+import Hero from '@/components/Hero'
+import Categories from '@/components/Categories'
 
 export default function Home() {
   return (
     <main>
       <h1 className={s.h1}>schussfreude.ch</h1>
       <p className={s.p}>Vorschau der neuen schussfreude.ch-Seite.</p>
+      <Hero />
+      <Categories />
     </main>
   )
 }
