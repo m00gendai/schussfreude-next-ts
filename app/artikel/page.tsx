6f616a1ab2a6448c01671f66ace87b5b06222259
Marcel Weber <mrweber@gmx.ch>
2023-08-13 11:52:04 +0200

    Added static pages

---
+++ app/artikel/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Link from "next/link"
+import React from 'react'
+
+export default function Artikel() {
+  return (
+    <main>
+      <h1>Artikel</h1>
+    </main>
+  )
+}
