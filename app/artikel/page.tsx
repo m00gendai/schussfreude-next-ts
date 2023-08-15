0b71f81d65949b57173daf00988dfaa7fef9b1b6
Marcel Weber <mrweber@gmx.ch>
2023-08-15 21:45:20 +0200

    Artikel page structure

--- app/artikel/page.tsx
+++ app/artikel/page.tsx
@@ -1,11 +1,33 @@
 import Image from 'next/image'
 import Link from "next/link"
 import React from 'react'
+import {Book,Medium} from "@/interfaces/interface_Book"
+import s from "@/styles/artikel.module.css"
+import ArticleGallery from '@/components/ArticleGallery'
 
-export default function Artikel() {
+async function getBooks(){
+  const getData = await fetch(`https://cms.schussfreude.ch/api/content/items/books?populate=1000&limit=5`,{
+    "headers": {
+      "api-key": process.env.CMS!,
+    }
+  ,
+  next: { revalidate: 10 } }) // TODO: Increase in prod
+  
+  return await getData.json()
+}
+
+export default async function Artikel() {
+
+  const books:Book[] = await getBooks()
+
+  const articles:(Book[])= [...books]
+
   return (
     <main>
       <h1>Artikel</h1>
+      <section>
+        <ArticleGallery articles={articles} />
+      </section>
     </main>
   )
 }
