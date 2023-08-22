6956ccd388546f3657e6633863a0e0788777cf95
Marcel Weber <mrweber@gmx.ch>
2023-08-22 18:26:46 +0200

    Fetching of categories directly in page Added tabbed category/article view

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,21 @@ import s from "@/styles/frontpage.module.css"
 import Hero from '@/components/Hero'
 import Categories from '@/components/Categories'
 import Link from "next/link"
-import {Book} from "@/interfaces/interface_Book"
+import {Book, Tag} from "@/interfaces/interface_Book"
 import {Misc} from "@/interfaces/interface_Misc"
 import { sortDataByDate } from '@/utils'
+import Tab from '@/components/Tab'
 
+async function getCategories(){
+  const getData = await fetch(`https://cms.schussfreude.ch/api/content/items/tags?populate=1`,{
+    "headers": {
+      "api-key": process.env.CMS!
+    }  
+  ,
+  next: { revalidate: 10 } }) // TODO: Increase in prod
+  
+  return await getData.json()
+}
 
 //&sort=%7B_created%3A-1%7D
 async function getBooks(){
@@ -36,6 +47,7 @@ export default async function Home() {
 
   const books:Book[] = await getBooks()
   const misc:Misc[] = await getMisc()
+  const cats:Tag[] = await getCategories()
 
   const articles:(Book|Misc)[]= [...books, ...misc].sort((a,b) => sortDataByDate(a, b))
 
@@ -51,9 +63,11 @@ export default async function Home() {
         Github Repository (Englisch)
       </Link>
       {/* TODO: This will remain in prod. Delete comment then */}
-      <Hero articles={articles}/>
-      {/* @ts-expect-error Server Component */}
-      <Categories />
+      <div className={s.desktop}>
+        <Hero articles={articles}/>
+        <Categories cats={cats}/>
+      </div>
+      <Tab articles={articles} cats={cats}/>
     </main>
   )
 }
