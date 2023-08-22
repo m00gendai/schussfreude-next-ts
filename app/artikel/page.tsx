f7c91555d1bbb0e7c91677785da4a47483a0344b
Marcel Weber <mrweber@gmx.ch>
2023-08-22 16:38:10 +0200

    Incorporated Misc API data

--- app/artikel/page.tsx
+++ app/artikel/page.tsx
@@ -1,12 +1,24 @@
 import Image from 'next/image'
 import Link from "next/link"
 import React from 'react'
-import {Book,Medium} from "@/interfaces/interface_Book"
+import {Book} from "@/interfaces/interface_Book"
+import {Misc} from "@/interfaces/interface_Misc"
 import s from "@/styles/artikel.module.css"
 import ArticleGallery from '@/components/ArticleGallery'
+import { sortDataByDate } from '@/utils'
 
 async function getBooks(){
-  const getData = await fetch(`https://cms.schussfreude.ch/api/content/items/books?populate=1000&limit=5`,{
+  const getData = await fetch(`https://cms.schussfreude.ch/api/content/items/books?populate=1`,{
+    "headers": {
+      "api-key": process.env.CMS!,
+    }
+  ,
+  next: { revalidate: 10 } }) // TODO: Increase in prod
+  
+  return await getData.json()
+}
+async function getMisc(){
+  const getData = await fetch(`https://cms.schussfreude.ch/api/content/items/misc?populate=1`,{
     "headers": {
       "api-key": process.env.CMS!,
     }
@@ -19,8 +31,9 @@ async function getBooks(){
 export default async function Artikel() {
 
   const books:Book[] = await getBooks()
+  const misc:Misc[] = await getMisc()
 
-  const articles:(Book[])= [...books]
+  const articles:(Book|Misc)[] = [...books, ...misc].sort((a,b) => sortDataByDate(a, b))
 
   return (
     <main>
