43b8fb5e7b29d43dfe37f1a9f1631fea72714b04
Marcel Weber <mrweber@gmx.ch>
2023-09-07 09:46:39 +0200

    Added function to convert hex to rgb Added function to generate a placeholder gradient style object

--- utils.ts
+++ utils.ts
@@ -102,3 +102,24 @@ export function magazineUrlReplacer(string:string){
         .replaceAll("(", "")
         .replaceAll(")", "")
 }
+
+export function toRGB(hex: string) {
+    const r:number = parseInt(hex.slice(1, 3), 16);
+    const g:number = parseInt(hex.slice(3, 5), 16);
+    const b:number = parseInt(hex.slice(5, 7), 16);
+    const rgb:string = `${r},${g},${b}`;
+    return rgb;
+  }
+
+  export function gradientPlaceholder(rgb:string[]){
+    const style:React.CSSProperties = {
+        background: `
+            linear-gradient(72deg, rgba(${rgb[0]},0.8), rgba(${rgb[0]}, 0) 70.71%), 
+            linear-gradient(144deg, rgba(${rgb[1]},0.8), rgba(${rgb[1]}, 0) 70.71%),
+            linear-gradient(216deg, rgba(${rgb[2]},0.8), rgba(${rgb[2]}, 0) 70.71%),
+            linear-gradient(288deg, rgba(${rgb[3]},0.8), rgba(${rgb[3]}, 0) 70.71%),
+            linear-gradient(360deg, rgba(${rgb[4]},0.8), rgba(${rgb[4]}, 0) 70.71%)
+            `,
+    }
+    return style
+  }
