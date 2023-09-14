ce963a05491f87cab075c5a86e4960f5f7e389c8
Marcel Weber <mrweber@gmx.ch>
2023-09-14 10:58:13 +0200

    Created Provider component for Cookie bar and Cookie handling

---
+++ components/CookieProvider.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { CookieConsent, getCookieConsentValue } from "react-cookie-consent";
+import { useEffect } from "react"
+import ReactGA from "react-ga4";
+import Link from "next/link"
+
+import c from "@/styles/CookieProvider.module.css"
+
+interface Props{
+    children: React.ReactNode
+}
+
+export default function CookieProvider({children}:Props){
+
+    useEffect(() => {
+        const isConsent = getCookieConsentValue("schussfreude_analytics");
+        if (isConsent === "true") {
+          handleAcceptCookie();
+        }
+      }, []);
+    
+      const initGA = (id: string) => {
+          ReactGA.initialize(id);
+      };
+
+      const handleAcceptCookie = () => {
+        initGA(`${process.env.NEXT_PUBLIC_GA}`)
+    };
+  
+    return(
+        <>
+            {children}
+            <CookieConsent
+                location="bottom"
+                buttonText="Jo!"
+                cookieName="schussfreude_analytics"
+                style={{background: "white"}}
+                containerClasses={c.bar}
+                contentClasses={c.inner}
+                buttonWrapperClasses={c.buttons}
+                buttonClasses={c.accept}
+                declineButtonClasses={c.decline}
+                enableDeclineButton
+                declineButtonText="Nä-ä!"
+                onAccept={handleAcceptCookie}
+                disableStyles
+                disableButtonStyles
+            >
+                <span className={c.span}>
+                    Diese Webseite nutzt Cookies für die Analyse von Besucherverhalten! 
+                </span>
+                <Link 
+                    style={{margin: "0 1rem" }} 
+                    title="Impressum"  
+                    href="/impressum"
+                >
+                    Impressum
+                </Link>
+                <Link 
+                    style={{margin: "0 1rem" }}
+                    title="Datenschutzerklärung" 
+                    href="/dsgvo"
+                >
+                    Datenschutzerklärung
+                </Link>
+            </CookieConsent>
+        </>
+    )
+}
