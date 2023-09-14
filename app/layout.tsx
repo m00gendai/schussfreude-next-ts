039da1bd2b35b12ba261e47c445d6ff8f6e8514c
Marcel Weber <mrweber@gmx.ch>
2023-09-14 10:58:28 +0200

    Incorporated CookieProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar"
 import Navbar_Mobile from '@/components/Navbar_Mobile'
 import Footer from '@/components/Footer'
 import Footer_Mobile from '@/components/Footer_Mobile'
+import CookieProvider from "@/components/CookieProvider"
 
 export default function RootLayout({
   children,
@@ -15,7 +16,9 @@ export default function RootLayout({
       <body>
         <Navbar_Mobile />
         <Navbar />
-        {children}
+        <CookieProvider>
+          {children}
+        </CookieProvider>
         <Footer />
         <Footer_Mobile />
       </body>
